24adfaabd6c431da7948030d3afd312f921595fa
dbasumatary <basudhondip@gmail.com>
2023-02-02 20:56:57 +0530

    Added: Part time employee and their wage

--- src/com/employeewage/EmployeeWageComputation.java
+++ src/com/employeewage/EmployeeWageComputation.java
@@ -2,7 +2,8 @@ package com.employeewage;
 public class EmployeeWageComputation {
     public static void main(String[] args) {
         // Constants
-        int IS_FULL_TIME = 1;
+        int IS_PART_TIME = 1;
+        int IS_FULL_TIME = 2;
         int EMP_RATE_PER_HOUR = 20;
 
         //Variables
@@ -10,10 +11,14 @@ public class EmployeeWageComputation {
         int empWage;
 
         // Computation
-        double empCheck = Math.floor(Math.random() * 10) % 2;
-        if (empCheck == IS_FULL_TIME) {
+        double empCheck = Math.floor(Math.random() * 10) % 3;
+        if (empCheck == IS_PART_TIME) {
+            empHrs = 4;
+        }
+        else if (empCheck == IS_FULL_TIME){
             empHrs = 8;
-        } else {
+        }
+        else {
             empHrs = 0;
         }
         empWage = empHrs * EMP_RATE_PER_HOUR;
