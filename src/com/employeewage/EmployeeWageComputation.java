b0e90f554e76565c16af9d591926e44ff05d44be
dbasumatary <basudhondip@gmail.com>
2023-02-02 23:29:49 +0530

    Added: Program to calculate wages till 100 hours and 20 days is reached for a month

--- src/com/employeewage/EmployeeWageComputation.java
+++ src/com/employeewage/EmployeeWageComputation.java
@@ -4,14 +4,16 @@ public class EmployeeWageComputation {
     public static final int IS_FULL_TIME = 2;
     public static final int EMP_RATE_PER_HOUR = 20;
     public static final int NUM_OF_WORKING_DAYS = 20;
+    public static final int MAX_HRS_IN_MONTH = 100;
     public static void main(String[] args) {
         //Variables
-        int empHrs;
-        int empWage;
-        int totalEmpWage = 0;
+        int totalEmpHrs = 0;
+        int totalWorkingDays = 0;
 
-        // Computation of wages for the month assuming 20 working days
-        for (int day = 0; day < NUM_OF_WORKING_DAYS; day++){
+        // Computation of wages till 100 hours and 20 days is reached
+        while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS){
+            int empHrs;
+            totalWorkingDays++;
             int empCheck = (int) Math.floor(Math.random() * 10) % 3;
             switch (empCheck) {
                 case IS_FULL_TIME:
@@ -23,10 +25,10 @@ public class EmployeeWageComputation {
                 default:
                     empHrs = 0;
             }
-            empWage = empHrs * EMP_RATE_PER_HOUR;
-            totalEmpWage += empWage;
-            System.out.println("Emp Wage: " + empWage);
+            totalEmpHrs += empHrs;
+            System.out.println("Day:" + totalWorkingDays + " Employee Working Hours: " +empHrs);
         }
-        System.out.println("Total Emp Wage: " + totalEmpWage);
+        int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+        System.out.println("Total Employee Wage: " + totalEmpWage);
     }
 }
