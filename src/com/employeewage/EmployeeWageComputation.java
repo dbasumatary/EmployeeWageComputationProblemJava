e2ebee86daba373a8029c03b09ded2ba3a7ab167
dbasumatary <basudhondip@gmail.com>
2023-02-06 19:36:11 +0530

    Added: Check whether employee is present or absent

--- src/com/employeewage/EmployeeWageComputation.java
+++ src/com/employeewage/EmployeeWageComputation.java
@@ -2,9 +2,9 @@ package com.employeewage;
 public class EmployeeWageComputation {
     public static void main(String[] args) {
         // Constants instantiation
-        int isFullTime = 1;
+        int IS_FULL_TIME = 1;
         double empCheck = Math.floor(Math.random() * 10) % 2;
-        if (empCheck == isFullTime) {
+        if (empCheck == IS_FULL_TIME) {
             System.out.println("Employee is Present");
         }
         else{
